fb52c851449a4e46b72327d490a1ab43724d8d31
Nickolay Abdrafikov <nicck.olay@gmail.com>
2013-08-17 17:35:05 +0400

    extract statusBar, trayMenu and quitItem methods

--- main.rb
+++ main.rb
@@ -2,28 +2,45 @@ framework 'AppKit'
 
 class AppDelegate
   def applicationDidFinishLaunching(notification)
-    statusBar = NSStatusBar
-        .systemStatusBar
-        .statusItemWithLength(NSVariableStatusItemLength)
+    puts 'Hi!'
 
-    item = NSMenuItem.new
-    item.title = 'Quit'
-    item.target = self
-    item.action = 'quit:'
-
-    menu = NSMenu.new
-    menu.addItem(item)
-
-    statusBar.menu = menu
-    statusBar.title = "CI Menu"
-    statusBar.highlightMode = true
-
-    puts 'started'
+    statusBar.menu = trayMenu
+    trayMenu.addItem(quitItem)
   end
 
   def quit(notification)
-    puts "Bye!"
+    puts 'Bye!'
     exit
+  end
+
+  private
+
+  def statusBar
+    @statusBar ||= begin
+      statusBar = NSStatusBar
+        .systemStatusBar
+        .statusItemWithLength(NSVariableStatusItemLength)
+      statusBar.title = "CI Menu"
+      statusBar.highlightMode = true
+      statusBar
+    end
+  end
+
+  def trayMenu
+    @trayMenu ||= begin
+      menu = NSMenu.new
+      menu
+    end
+  end
+
+  def quitItem
+    @quitItem ||= begin
+      item = NSMenuItem.new
+      item.title = 'Quit'
+      item.target = self
+      item.action = 'quit:'
+      item
+    end
   end
 end
 
