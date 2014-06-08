c9a95d106b5ea767af0f0d569e3d4440aa680089
Nickolay Abdrafikov <nicck.olay@gmail.com>
2014-06-09 00:40:08 +0400

    Initial Commit

---
+++ CIMenu/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CIMenu
+//
+//  Created by Nickolay Abdrafikov on 09.06.14.
+//  Copyright (c) 2014 Nickolay Abdrafikov. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
